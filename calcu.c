01db44180618423845ce72d7d4f190aabf32192b
noble <nblphilip7@gmail.com>
2024-07-22 19:21:43 +0530

    added division

--- calcu.c
+++ calcu.c
@@ -7,6 +7,7 @@ int main()
 	scanf("%d %d", &n1, &n2);
 	printf("%d + %d = %d \n", n1, n2, n1+n2);
 	printf("%d - %d = %d \n", n1, n2, n1-n2);
+	printf("%d / %d = %d \n", n1, n2, n1/n2);
 	return 0;
 }
 
